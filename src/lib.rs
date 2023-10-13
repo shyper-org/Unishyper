cd222c86b53b77f0c1d5eb43f404f055baee1613
saki <1059830755@qq.com>
2023-10-13 17:05:36 +0800

    revert

--- src/lib.rs
+++ src/lib.rs
@@ -90,7 +90,7 @@ pub use exported::*;
 // This `irq_disable` is just for test, to be moved.
 pub use arch::irq::disable as irq_disable;
 pub use mm::heap::Global;
-
+pub use arch::page_table;
 pub use panic::random_panic;
 
 pub(crate) const MASTER_CPU_ID: usize = 0;
@@ -137,22 +137,22 @@ pub extern "C" fn loader_main(core_id: usize) {
         #[cfg(feature = "smp")]
         board::launch_other_cores();
     }
-
+    
     board::init_per_core();
     // // Init schedule for per core.
     libs::scheduler::init();
 
     if core_id == MASTER_CPU_ID {
         board::init();
-
+        
         #[cfg(feature = "net")]
         libs::net::init();
         #[cfg(feature = "fs")]
         libs::fs::init();
-
+        
         // #[cfg(feature = "zone")]
         zone::zone_init();
-
+        
         info!("board init ok");
         #[cfg(not(feature = "std"))]
         extern "Rust" {
@@ -186,7 +186,7 @@ pub extern "C" fn loader_main(core_id: usize) {
         #[cfg(feature = "terminal")]
         libs::terminal::init();
     }
-
+    
     // Enter first thread.
     // On core 0, this should be user's main thread.
     // On other cores, this may be idle thread.
